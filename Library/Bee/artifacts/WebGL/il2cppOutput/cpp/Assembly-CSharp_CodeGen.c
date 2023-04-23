ad01680e414487f7e94e23bb217ccfd61a5f7a67
Vishal <shrivastava_vishal@outlook.com>
2023-04-23 10:01:14 -0500

    stable version - published v1.0

---
+++ Library/Bee/artifacts/WebGL/il2cppOutput/cpp/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,77 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void RealtimeReflection::Awake()
+extern void RealtimeReflection_Awake_m3FBF61FC4218CA1345AB04B392BECE309350A1C6 (void);
+// 0x00000002 System.Void RealtimeReflection::Update()
+extern void RealtimeReflection_Update_mD8E8DCD472EE837CF30A93DCBFEE9169FBBC4485 (void);
+// 0x00000003 System.Void RealtimeReflection::.ctor()
+extern void RealtimeReflection__ctor_mCDFF2DE57B33199E32E7E3B127B87611187E6096 (void);
+// 0x00000004 System.Void RealtimeReflection1::Awake()
+extern void RealtimeReflection1_Awake_m81C3425B420205CD0DFA54046D477DD400D814F4 (void);
+// 0x00000005 System.Void RealtimeReflection1::Update()
+extern void RealtimeReflection1_Update_mCCCC8C29A3E181F96AD273D416661F162867DE18 (void);
+// 0x00000006 System.Void RealtimeReflection1::.ctor()
+extern void RealtimeReflection1__ctor_mBFE1027E356257C5BC6F1217270F1BE5EC976A3E (void);
+// 0x00000007 System.Void NewBehaviourScript::FixedUpdate()
+extern void NewBehaviourScript_FixedUpdate_m5CD29F5FE80BB8313BEF43B4327D608E666F3D86 (void);
+// 0x00000008 System.Void NewBehaviourScript::OnCollisionEnter(UnityEngine.Collision)
+extern void NewBehaviourScript_OnCollisionEnter_m4FBC73AE72EBCA0EB6640BBD5655914CBDD773F9 (void);
+// 0x00000009 System.Void NewBehaviourScript::.ctor()
+extern void NewBehaviourScript__ctor_mD2E080DE77BCDB61B6D2EC8AD996FAE611B97F3C (void);
+static Il2CppMethodPointer s_methodPointers[9] = 
+{
+	RealtimeReflection_Awake_m3FBF61FC4218CA1345AB04B392BECE309350A1C6,
+	RealtimeReflection_Update_mD8E8DCD472EE837CF30A93DCBFEE9169FBBC4485,
+	RealtimeReflection__ctor_mCDFF2DE57B33199E32E7E3B127B87611187E6096,
+	RealtimeReflection1_Awake_m81C3425B420205CD0DFA54046D477DD400D814F4,
+	RealtimeReflection1_Update_mCCCC8C29A3E181F96AD273D416661F162867DE18,
+	RealtimeReflection1__ctor_mBFE1027E356257C5BC6F1217270F1BE5EC976A3E,
+	NewBehaviourScript_FixedUpdate_m5CD29F5FE80BB8313BEF43B4327D608E666F3D86,
+	NewBehaviourScript_OnCollisionEnter_m4FBC73AE72EBCA0EB6640BBD5655914CBDD773F9,
+	NewBehaviourScript__ctor_mD2E080DE77BCDB61B6D2EC8AD996FAE611B97F3C,
+};
+static const int32_t s_InvokerIndices[9] = 
+{
+	980,
+	980,
+	980,
+	980,
+	980,
+	980,
+	980,
+	857,
+	980,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	9,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
