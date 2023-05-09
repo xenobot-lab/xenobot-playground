3e91fba8c95c51c39654bbfba9bbb41a9544b4f0
Vishal <shrivastava_vishal@outlook.com>
2023-05-09 13:19:58 -0500

    unfreeze rotation when gravity is off

--- Assets/GravityScript.cs
+++ Assets/GravityScript.cs
@@ -7,56 +7,30 @@ using UnityEngine.Events;
 public class GravityScript : MonoBehaviour
 {
     public Button gravityButton;
-    // private Rigidbody rb;
     private GameObject[] myObjects;
     private bool isGravityEnabled = true;
-    // private var colors = GetComponent<Button> ().colors;
-    // private ColorBlock color1;
-    // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("i am in start");
-        // rb = GetComponent<Rigidbody>();
-        // List<GameObject>[] gameObjects=GameObject.FindObjectsOfType(typeof(GameObject)) as GameObject[];
-        // gravityButton.onClick.AddListener(UnityEngine.PlayerLoop.Update);
-        myObjects = GameObject.FindGameObjectsWithTag("bot") as GameObject[];
-
-        for (var i = 0; i < myObjects.Length; i++) {
-            if (myObjects[i].tag != "control"){
-                gravityChange(myObjects[i]);
-            }
-        }
-        // Debug.Log(rb.name);
-        GetComponent<Button>().onClick.AddListener(changeColor);
-    }
-
-    // Update is called once per frame
-    void changeColor(){
-        isGravityEnabled = !isGravityEnabled;
-        // ColorBlock color1 = GetComponent<Button>().colors;
-        if (isGravityEnabled){
-            // color1.normalColor = Color.green;
-            // GetComponent<Button>().colors = color1;
-            GetComponent<Image>().color = Color.green;
-        }
-
-        else {
-            // color1.normalColor = Color.red;
-            // GetComponent<Button>().colors = color1;
-            GetComponent<Image>().color = Color.red;
-        }
-
+        GetComponent<Button>().onClick.AddListener(changeGravityConfig);
         
     }
 
-    // Update is called once per frame
-    void gravityChange(GameObject rb)
+    void changeGravityConfig()
     {
-        Debug.Log("i am in update");
-        // isGravityEnabled = !isGravityEnabled;
-        rb.GetComponent<Rigidbody>().useGravity = isGravityEnabled;
-        // Debug.Log(gravityButton);
-        // GetComponentInChildren<Text>()
-        //= isGravityEnabled ? "Disable Gravity" : "Enable Gravity";
+        isGravityEnabled = !isGravityEnabled;
+        GetComponent<Image>().color = (isGravityEnabled) ? Color.green : Color.red;
+        myObjects = GameObject.FindGameObjectsWithTag("bot") as GameObject[];
+
+        for (var i = 0; i < myObjects.Length; i++) {
+            changeConstraints(myObjects[i]);
+        }
+    }
+
+    void changeConstraints(GameObject g)
+    {
+        g.GetComponent<Rigidbody>().useGravity = isGravityEnabled;
+        g.GetComponent<Rigidbody>().constraints = (isGravityEnabled) ?
+        RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ :
+        RigidbodyConstraints.None;
     }
 }
